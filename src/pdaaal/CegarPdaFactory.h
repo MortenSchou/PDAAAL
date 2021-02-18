32e66a8a269bb5d36925de5e6eb34d3e19170478
Morten Schou <mksc@cs.aau.dk>
2021-02-18 18:59:26 +0100

    Log post* time. Temporary

--- src/pdaaal/CegarPdaFactory.h
+++ src/pdaaal/CegarPdaFactory.h
@@ -1,4 +1,65 @@
-/* 
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// originates from the verifypn-project.
+
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
+
+#include <ctime>
+#include <sstream>
+#include <chrono>
+
+class stopwatch {
+    using clock = std::chrono::high_resolution_clock;
+    bool _running = false;
+
+    clock::time_point _start;
+    clock::time_point _stop;
+
+public:
+    auto started() const { return _start; }
+    auto stopped() const { return _stop; }
+    bool running() const { return _running; }
+    stopwatch(bool r = true) : _running(r), _start(clock::now()), _stop(clock::now()){}
+    void start() {
+        if(!_running)
+        {
+            _running = true;
+            _start = clock::now() - (_stop - _start);
+        }
+    }
+    void stop() {
+        if(_running)
+        {
+            _stop = clock::now();
+            _running = false;
+        }
+    }
+
+    double duration() const { return std::chrono::duration_cast<std::chrono::duration<double>>(_stop-_start).count(); }
+
+    std::ostream &operator<<(std::ostream &os){
+        os << duration() << " s";
+        return os;
+    }
+};
+
+#endif // STOPWATCH_H
+
+/*
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -440,7 +501,6 @@ namespace pdaaal {
     };
 
 
-
     template <typename Factory, typename Reconstruction>
     class CEGAR {
         static_assert(std::is_same_v<typename Factory::label_t, typename Reconstruction::label_t>);
@@ -451,9 +511,11 @@ namespace pdaaal {
     public:
         template<bool use_pre_star=false, bool use_dual_star=false>
         std::optional<concrete_trace_t> cegar_solve(Factory&& factory, const NFA<label_t>& initial_headers, const NFA<label_t>& final_headers) {
+            stopwatch post_star_clock(false);
             while(true) {
                 auto instance = factory.compile(initial_headers, final_headers);
 
+                post_star_clock.start();
                 bool result;
                 if constexpr (use_pre_star) {
                     result = Solver::pre_star_accepts(instance);
@@ -462,6 +524,7 @@ namespace pdaaal {
                 } else {
                     result = Solver::post_star_accepts(instance);
                 }
+                post_star_clock.stop();
                 if (!result) {
                     return std::nullopt; // No trace.
                 }
@@ -471,6 +534,7 @@ namespace pdaaal {
                 auto res = reconstruction.template reconstruct_trace<use_dual_star>();
 
                 if (std::holds_alternative<concrete_trace_t>(res)) {
+                    std::cerr << "Accumulated time for post*: " << post_star_clock.duration() << std::endl;
                     return std::get<concrete_trace_t>(res);
                 } else if (std::holds_alternative<refinement_t>(res)) {
                     if (std::get<refinement_t>(res).index() == 0) {
