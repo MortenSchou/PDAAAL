4a95fb3f02d1d9bf3f835db3371e4891894fbc55
Morten Schou <mksc@cs.aau.dk>
2021-10-29 18:49:09 +0200

    Implement: Typed PDA return all rules

--- src/include/pdaaal/TypedPDA.h
+++ src/include/pdaaal/TypedPDA.h
@@ -69,6 +69,11 @@ namespace pdaaal {
             size_t _to = std::numeric_limits<size_t>::max();
             op_t _op = POP;
             label_t _op_label;
+
+            rule_t_() = default;
+            rule_t_(const TypedPDA& pda, size_t from, uint32_t pre, details::rule_t<WT> rule)
+            : _from(from), _pre(pda.get_symbol(pre)), _to(rule._to), _op(rule._operation),
+              _op_label(rule._operation == POP || rule._operation == NOOP ? label_t{} : pda.get_symbol(rule._op_label)) {};
         };
         template <typename WT>
         struct rule_t_<WT, std::enable_if_t<is_weighted<WT>>> {
@@ -78,6 +83,12 @@ namespace pdaaal {
             op_t _op = POP;
             label_t _op_label;
             typename WT::type _weight;
+
+            rule_t_() = default;
+            rule_t_(const TypedPDA& pda, size_t from, uint32_t pre, details::rule_t<WT> rule)
+            : _from(from), _pre(pda.get_symbol(pre)), _to(rule._to), _op(rule._operation),
+              _op_label(rule._operation == POP || rule._operation == NOOP ? label_t{} : pda.get_symbol(rule._op_label)),
+              _weight(rule._weight) {};
         };
     public:
         using rule_t = rule_t_<W>; // This rule type can be used by users of the library.
@@ -184,6 +195,26 @@ namespace pdaaal {
         }
         void add_rule_detail(size_t from, typename PDA<W>::rule_t r, bool negated, const std::vector<uint32_t>& pre) {
             this->add_untyped_rule_impl(from, r, negated, pre);
+        }
+
+        std::vector<rule_t> all_rules() const {
+            std::vector<rule_t> result;
+            size_t state_i = 0;
+            for (const auto& state : this->states()) {
+                for (const auto& [rule,labels] : state._rules) {
+                    if (labels.wildcard()) {
+                        for (uint32_t pre = 0; pre < number_of_labels(); ++pre) {
+                            result.emplace_back(*this, state_i, pre, rule);
+                        }
+                    } else {
+                        for (const auto& pre : labels.labels()) {
+                            result.emplace_back(*this, state_i, pre, rule);
+                        }
+                    }
+                }
+                ++state_i;
+            }
+            return result;
         }
 
     protected:
